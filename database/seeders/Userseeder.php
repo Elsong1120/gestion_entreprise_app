999b159f2d53e988d7f9ab4b91f0bc44f94c1463
Elsong1120 <elsondushime@gmail.com>
2022-06-17 18:15:36 +0200

    creation Userseeder

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('oklm'),
+                'isprofilcompleted' => false,
+            ],
+            [
+
+                'name' => 'oklm',
+                'email' => 'oklm@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('test'),
+                'isprofilcompleted' => true,
+            ],
+        ]);
+    }
+}
